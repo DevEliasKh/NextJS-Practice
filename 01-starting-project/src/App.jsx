99e379c550ab92a60954be44131a909f69443403
develiaskh <develiaskh@gmail.com>
2025-11-23 21:11:05 +0330

    feat: finish practice

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
+import UserInputs from "./components/UserInputs";
+import Result from "./components/Result";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    investmentDuration: 10,
+  });
+
+  const inputIsValid = userInput.investmentDuration > 0;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevData) => {
+      return { ...prevData, [inputIdentifier]: +newValue };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInputs userInput={userInput} onChange={handleChange} />
+      {inputIsValid ? (
+        <Result userInput={userInput} />
+      ) : (
+        <p>Please add a proper duration</p>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
